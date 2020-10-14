e8b4635c46b33ad9f1b2e5c9a66b53e678ac2fb3
Acer <pamoshakishani@gmail.com>
2020-10-14 15:16:29 +0530

    View order by supplier,respond interfaces

---
+++ app/src/main/java/com/crave/food/csse_android_app/OrderPlaceSitemanager.java
@@ -0,0 +1,14 @@
+package com.crave.food.csse_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderPlaceSitemanager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_place_sitemanager);
+    }
+}
--- app/src/main/java/com/crave/food/csse_android_app/OrderViewSupplier.java
+++ app/src/main/java/com/crave/food/csse_android_app/OrderViewSupplier.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class OrderPlace extends AppCompatActivity {
+public class OrderViewSupplier extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_place);
+        setContentView(R.layout.activity_order_view_supplier);
     }
 }
