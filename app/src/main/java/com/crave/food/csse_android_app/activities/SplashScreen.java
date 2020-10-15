8a509d67287e6347faa9f1bda60655aa53630746
samesh97 <Alahakoon123@>
2020-10-15 18:58:02 +0530

    added splash screen

---
+++ app/src/main/java/com/crave/food/csse_android_app/activities/SplashScreen.java
@@ -0,0 +1,49 @@
+package com.crave.food.csse_android_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.crave.food.csse_android_app.R;
+import com.crave.food.csse_android_app.config.LoginState;
+import com.crave.food.csse_android_app.models.Manager;
+import com.crave.food.csse_android_app.models.Supplier;
+import com.crave.food.csse_android_app.models.User;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent intent;
+                User user = LoginState.getUser(SplashScreen.this);
+
+                if(user instanceof Supplier)
+                {
+                    intent = new Intent(SplashScreen.this,SupplierLoggedActivity.class);
+                }
+                else if(user instanceof Manager)
+                {
+                    intent = new Intent(SplashScreen.this,ManagerLoggedActivity.class);
+                }
+                else
+                {
+                    intent = new Intent(SplashScreen.this,Login.class);
+                }
+                startActivity(intent);
+                finish();
+
+            }
+        },2000);
+
+    }
+}
