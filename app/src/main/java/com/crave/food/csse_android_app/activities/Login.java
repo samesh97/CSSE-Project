3cf513982d0244a8d77deeb711af4cf3168c69dd
samesh97 <Alahakoon123@>
2020-10-15 14:15:05 +0530

    Login by code

--- app/src/main/java/com/crave/food/csse_android_app/activities/Login.java
+++ app/src/main/java/com/crave/food/csse_android_app/activities/Login.java
@@ -1,16 +1,95 @@
 package com.crave.food.csse_android_app.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.crave.food.csse_android_app.R;
+import com.crave.food.csse_android_app.models.Manager;
+import com.crave.food.csse_android_app.models.Supplier;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
+
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        checkSupplierLogin("holcim@yahoo.com","123456");
+    }
+
+    public void checkSupplierLogin(final String email, final String password)
+    {
+        reference.child("Suppliers").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                if(snapshot.exists())
+                {
+                    for(DataSnapshot dss : snapshot.getChildren())
+                    {
+                        Supplier supplier = dss.getValue(Supplier.class);
+                        if(supplier != null && supplier.getSupplierEmail().equals(email) && supplier.getPassword().equals(password))
+                        {
+                            Toast.makeText(Login.this, "Success", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(Login.this, "No path found", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void checkManagerLogin(final String email, final String password)
+    {
+        reference.child("Managers").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                if(snapshot.exists())
+                {
+                    for(DataSnapshot dss : snapshot.getChildren())
+                    {
+                        Manager manager = dss.getValue(Manager.class);
+                        if(manager != null && manager.getEmail().equals(email) && manager.getPassword().equals(password))
+                        {
+                            Toast.makeText(Login.this, "success", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(Login.this, "Path doesn't exists", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/crave/food/csse_android_app/models/Manager.java
@@ -0,0 +1,47 @@
+package com.crave.food.csse_android_app.models;
+
+public class Manager
+{
+    private String companyId,email,managerName,password;
+    private long managerId;
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(long managerId) {
+        this.managerId = managerId;
+    }
+}
