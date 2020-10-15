d54a7a0519245a3223f853a7609dece7eafa4ba7
AILI <ailichandrasekara@gmail.com>
2020-10-15 17:31:13 +0530

    First Commit. Interfaces are created!

---
+++ app/src/main/java/com/crave/food/csse_android_app/PaymentCount.java
@@ -0,0 +1,14 @@
+package com.crave.food.csse_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentCount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_count);
+    }
+}
---
+++ app/src/main/java/com/crave/food/csse_android_app/PaymentView.java
@@ -0,0 +1,16 @@
+package com.crave.food.csse_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_view);
+    }
+
+
+}
