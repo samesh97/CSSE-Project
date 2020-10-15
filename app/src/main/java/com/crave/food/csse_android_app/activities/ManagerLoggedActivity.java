44c2af8b55e2da34fe8ab6d2ae1f00e9bfe76236
samesh97 <Alahakoon123@>
2020-10-15 18:53:06 +0530

    created activities

---
+++ app/src/main/java/com/crave/food/csse_android_app/activities/ManagerLoggedActivity.java
@@ -0,0 +1,16 @@
+package com.crave.food.csse_android_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.crave.food.csse_android_app.R;
+
+public class ManagerLoggedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_logged);
+    }
+}
