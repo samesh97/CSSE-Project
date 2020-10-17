a8334453abb4c005216fc1abf92e716ef6fafa98
chathura99124 <chathura749@gmail.com>
2020-10-18 05:26:07 +0530

    add and view

--- app/src/main/java/com/crave/food/csse_android_app/activities/SupplierLoggedActivity.java
+++ app/src/main/java/com/crave/food/csse_android_app/activities/SupplierLoggedActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.crave.food.csse_android_app.InvoicesMain;
 import com.crave.food.csse_android_app.OrderViewSitemanager;
@@ -12,10 +13,26 @@ import com.crave.food.csse_android_app.R;
 
 public class SupplierLoggedActivity extends AppCompatActivity {
 
+    Button add_receipt,view_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supplier_logged);
+
+        add_receipt = findViewById(R.id.add_reciept);
+
+
+
+        add_receipt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddReceipt();
+            }
+        });
+    }
+    public void openAddReceipt(){
+        Intent intent = new Intent(this,add_good_receipt.class);
+        startActivity(intent);
     }
 
 
---
+++ app/src/main/java/com/crave/food/csse_android_app/activities/View_good_receipt.java
@@ -0,0 +1,16 @@
+package com.crave.food.csse_android_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.crave.food.csse_android_app.R;
+
+public class View_good_receipt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_good_receipt);
+    }
+}
