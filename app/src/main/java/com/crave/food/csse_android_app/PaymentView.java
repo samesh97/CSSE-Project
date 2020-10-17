15041d7b12589b0872d4c38abcbc8322c078e243
AILI <ailichandrasekara@gmail.com>
2020-10-17 19:17:55 +0530

    Recyclerview Payment

--- app/src/main/java/com/crave/food/csse_android_app/PaymentView.java
+++ app/src/main/java/com/crave/food/csse_android_app/PaymentView.java
@@ -1,16 +1,40 @@
 package com.crave.food.csse_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.crave.food.csse_android_app.adapters.InvoicesAdapter;
+import com.crave.food.csse_android_app.adapters.PaymentAdapter;
+import com.crave.food.csse_android_app.models.Invoice;
+import com.crave.food.csse_android_app.models.Payment;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
 
 public class PaymentView extends AppCompatActivity {
+    DatabaseReference reference;
+    private ArrayList<Payment> paymentList;
+    private RecyclerView recyclerView;
+    private PaymentAdapter paymentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_view);
+
+        paymentList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerviewPayment);
+        paymentAdapter = new PaymentAdapter(PaymentView.this,paymentList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(PaymentView.this));
+        recyclerView.setAdapter(paymentAdapter);
     }
+
+
 
 
 }
---
+++ app/src/main/java/com/crave/food/csse_android_app/models/Payment.java
@@ -0,0 +1,31 @@
+package com.crave.food.csse_android_app.models;
+
+public class Payment {
+    private String heading;
+    private String count;
+    private String balance;
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+}
