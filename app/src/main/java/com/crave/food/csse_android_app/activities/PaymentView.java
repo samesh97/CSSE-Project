17e490e8765dbc43146917d6755cbf77c2e2126c
chathura99124 <chathura749@gmail.com>
2020-10-18 14:58:12 +0530

    add and view

--- app/src/main/java/com/crave/food/csse_android_app/activities/PaymentView.java
+++ app/src/main/java/com/crave/food/csse_android_app/activities/PaymentView.java
@@ -14,22 +14,22 @@ import com.google.firebase.database.DatabaseReference;
 import java.util.ArrayList;
 
 public class PaymentView extends AppCompatActivity {
-    DatabaseReference reference;
-    private ArrayList<Payment> paymentList;
-    private RecyclerView recyclerView;
-    private PaymentAdapter paymentAdapter;
+//    DatabaseReference reference;
+//    private ArrayList<Payment> paymentList;
+//    private RecyclerView recyclerView;
+//    private PaymentAdapter paymentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_view);
 
-        paymentList = new ArrayList<>();
-
-        recyclerView = findViewById(R.id.recyclerviewPayment);
-        paymentAdapter = new PaymentAdapter(PaymentView.this,paymentList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(PaymentView.this));
-        recyclerView.setAdapter(paymentAdapter);
+//        paymentList = new ArrayList<>();
+//
+//        recyclerView = findViewById(R.id.recyclerviewPayment);
+//        paymentAdapter = new PaymentAdapter(PaymentView.this,paymentList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(PaymentView.this));
+//        recyclerView.setAdapter(paymentAdapter);
 
 
     }
